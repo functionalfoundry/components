b453d813bfb27a439a705f08e53205261c88fada
Yaniv Tal <yaniv@workfloapp.com>
2016-11-20 18:13:40 -0800

    Change export style for index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,43 @@
-export Avatar from './Avatar'
-export Button from './Button'
-export Card from './Card'
-export Heading from './Heading'
-export Icon from './Icon'
-export Image from './Image'
-export Pointer from './Pointer'
-export Preview from './Preview'
-export PreviewContainer from './PreviewContainer/PreviewContainer'
-export Text from './Text'
-export TextInput from './TextInput'
-export View from './View'
-export Grid from './Grid'
+// export Avatar from './Avatar'
+// export Button from './Button'
+// export Card from './Card'
+// export Heading from './Heading'
+// export Icon from './Icon'
+// export Image from './Image'
+// export Pointer from './Pointer'
+// export Preview from './Preview'
+// export PreviewContainer from './PreviewContainer/PreviewContainer'
+// export Text from './Text'
+// export TextInput from './TextInput'
+// export View from './View'
+// export Grid from './Grid'
+
+import Avatar from './Avatar'
+import Button from './Button'
+import Card from './Card'
+import Heading from './Heading'
+import Icon from './Icon'
+import Image from './Image'
+import Pointer from './Pointer'
+import Preview from './Preview'
+import PreviewContainer from './PreviewContainer/PreviewContainer'
+import Text from './Text'
+import TextInput from './TextInput'
+import View from './View'
+import Grid from './Grid'
+
+module.exports = {
+  Avatar,
+  Button,
+  Card,
+  Heading,
+  Icon,
+  Image,
+  Pointer,
+  Preview,
+  PreviewContainer,
+  Text,
+  TextInput,
+  View,
+  Grid,
+}
