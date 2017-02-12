22c0a35c4a48ccfa463b8bfcd0fe5fab43232b3f
Yaniv Tal <yanivtalmusic@gmail.com>
2017-02-12 11:19:39 -0800

    Add Item and ItemsList from AutoWhatever

---
+++ src/AutoSuggest/ItemsList.js
@@ -0,0 +1,85 @@
+// Based on https://github.com/moroshko/react-autowhatever/blob/f4f83aabbd69a9cb2271e6f4efbb32bbe1a9544d/src/ItemsList.js
+
+import React, { Component, PropTypes } from 'react';
+import Item from './Item';
+import compareObjects from './compareObjects';
+
+export default class ItemsList extends Component {
+  static propTypes = {
+    items: PropTypes.array.isRequired,
+    itemProps: PropTypes.oneOfType([
+      PropTypes.object,
+      PropTypes.func
+    ]),
+    renderItem: PropTypes.func.isRequired,
+    renderItemData: PropTypes.object.isRequired,
+    sectionIndex: PropTypes.number,
+    focusedItemIndex: PropTypes.number,
+    onFocusedItemChange: PropTypes.func.isRequired,
+    getItemId: PropTypes.func.isRequired,
+    theme: PropTypes.func.isRequired,
+    keyPrefix: PropTypes.string.isRequired
+  };
+
+  static defaultProps = {
+    sectionIndex: null
+  };
+
+  constructor() {
+    super();
+
+    this.storeFocusedItemReference = this.storeFocusedItemReference.bind(this);
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return compareObjects(nextProps, this.props, ['itemProps']);
+  }
+
+  storeFocusedItemReference(focusedItem) {
+    this.props.onFocusedItemChange(focusedItem === null ? null : focusedItem.item);
+  }
+
+  render() {
+    const {
+      items, itemProps, renderItem, renderItemData, sectionIndex,
+      focusedItemIndex, getItemId, theme, keyPrefix
+    } = this.props;
+    const sectionPrefix = (sectionIndex === null ? keyPrefix : `${keyPrefix}section-${sectionIndex}-`);
+    const isItemPropsFunction = (typeof itemProps === 'function');
+
+    return (
+      <ul role="listbox" {...theme(`${sectionPrefix}items-list`, 'itemsList')}>
+        {
+          items.map((item, itemIndex) => {
+            const isFocused = (itemIndex === focusedItemIndex);
+            const itemKey = `${sectionPrefix}item-${itemIndex}`;
+            const itemPropsObj = isItemPropsFunction ? itemProps({ sectionIndex, itemIndex }) : itemProps;
+            const allItemProps = {
+              id: getItemId(sectionIndex, itemIndex),
+              ...theme(itemKey, 'item', isFocused && 'itemFocused'),
+              ...itemPropsObj
+            };
+
+            if (isFocused) {
+              allItemProps.ref = this.storeFocusedItemReference;
+            }
+
+            // `key` is provided by theme()
+            /* eslint-disable react/jsx-key */
+            return (
+              <Item
+                {...allItemProps}
+                sectionIndex={sectionIndex}
+                itemIndex={itemIndex}
+                item={item}
+                renderItem={renderItem}
+                renderItemData={renderItemData}
+              />
+            );
+            /* eslint-enable react/jsx-key */
+          })
+        }
+      </ul>
+    );
+  }
+}
