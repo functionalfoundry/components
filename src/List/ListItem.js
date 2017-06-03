3d104bff9428d5098e7f6a28ee703d319c55d51f
Brandon Ramirez <brandon.ramirez@live.com>
2017-06-02 18:18:16 -0700

    List: Add selection and hover behavior

--- src/List/ListItem.js
+++ src/List/ListItem.js
@@ -1,14 +1,13 @@
 /* @flow */
-
 import React from 'react'
-import Theme from 'js-theme'
-import { Colors, Fonts, Spacing } from '@workflo/styles'
 import View from '../View'
 
 type SizeT = 'Small' | 'Base' | 'Large'
 
 type PropsT = {
   children?: React.Children,
+  isSelected: boolean,
+  onClick: Function,
   size: SizeT,
   theme: Object,
 }
@@ -21,33 +20,16 @@ const defaultProps = {
  *  TODO: Remove last borderBottom using not pseudo selector
  */
 
-const ListItem = ({ children, theme, ...props }: PropsT) => (
-  <View {...props} {...theme.listItem}>
+const ListItem = ({ children, isSelected, onClick, theme, ...props }: PropsT) => (
+  <View
+    {...props}
+    {...(isSelected ? theme.selectedListItem : theme.listItem)}
+    onClick={onClick}
+  >
     {children}
   </View>
 )
 
-const defaultTheme = ({ size }: PropsT) => ({
-  listItem: {
-    ...getFont(size),
-    padding: Spacing.tiny,
-    borderBottom: `1px solid ${Colors.grey300}`,
-  },
-})
-
-const getFont = (size: SizeT) => {
-  switch (size) {
-    case 'Small':
-      return Fonts.small
-    case 'Large':
-      return Fonts.large
-    case 'Base':
-    default:
-      return Fonts.base
-  }
-}
-
 ListItem.defaultProps = defaultProps
 
-const ThemedListItem = Theme('ListItem', defaultTheme)(ListItem)
-export default ThemedListItem
+export default ListItem
