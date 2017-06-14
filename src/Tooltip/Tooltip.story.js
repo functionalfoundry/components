33f9ac765fe58dd4581c5ef06839dd7c732550b2
Brandon Ramirez <brandon.ramirez@live.com>
2017-06-13 20:09:30 -0700

    Tooltip: Add targetRef prop

--- src/Tooltip/Tooltip.story.js
+++ src/Tooltip/Tooltip.story.js
@@ -4,30 +4,56 @@ import Tooltip from '.'
 import PreviewContainer from '../PreviewContainer/PreviewContainer'
 import Preview from '../Preview'
 
-storiesOf('Tooltip', module).add('Regular', () => (
-  <PreviewContainer shade="dark">
-    <Preview label="Right" theme={{ content: styles.preview }}>
-      <Tooltip position="Right" portal={<div>Portal here</div>}>
-        Click Me
-      </Tooltip>
-    </Preview>
-    <Preview label="Bottom" theme={{ content: styles.preview }}>
-      <Tooltip position="Bottom" portal={<div>Portal here</div>}>
-        Click Me
-      </Tooltip>
-    </Preview>
-    <Preview label="Left" theme={{ content: styles.preview }}>
-      <Tooltip position="Left" portal={<div>Portal here</div>}>
-        Click Me
-      </Tooltip>
-    </Preview>
-    <Preview label="Top" theme={{ content: styles.preview }}>
-      <Tooltip position="Top" portal={<div>Portal here</div>}>
-        Click Me
-      </Tooltip>
-    </Preview>
-  </PreviewContainer>
-))
+storiesOf('Tooltip', module)
+  .add('Regular', () => (
+    <PreviewContainer shade="dark">
+      <Preview label="Right" theme={{ content: styles.preview }}>
+        <Tooltip position="Right" portal={<div>Portal here</div>}>
+          Click Me
+        </Tooltip>
+      </Preview>
+      <Preview label="Bottom" theme={{ content: styles.preview }}>
+        <Tooltip position="Bottom" portal={<div>Portal here</div>}>
+          Click Me
+        </Tooltip>
+      </Preview>
+      <Preview label="Left" theme={{ content: styles.preview }}>
+        <Tooltip position="Left" portal={<div>Portal here</div>}>
+          Click Me
+        </Tooltip>
+      </Preview>
+      <Preview label="Top" theme={{ content: styles.preview }}>
+        <Tooltip position="Top" portal={<div>Portal here</div>}>
+          Click Me
+        </Tooltip>
+      </Preview>
+    </PreviewContainer>
+  ))
+  .add('targetRef', () => <TooltipWithTargetRef />)
+
+class TooltipWithTargetRef extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      targetRef: null,
+    }
+  }
+  saveRefToTarget = targetRef => this.setState({ targetRef })
+  render() {
+    return (
+      <PreviewContainer>
+        <Preview label="Top" theme={{ content: styles.preview }}>
+          <div ref={this.saveRefToTarget}>Click Me </div>
+          <Tooltip
+            position="Top"
+            portal={<div>Portal here</div>}
+            targetRef={this.state.targetRef}
+          />
+        </Preview>
+      </PreviewContainer>
+    )
+  }
+}
 
 const commonStyle = {
   padding: 8,
