62b872a06ef7b4ce582657a8048d114d2b6ba128
Brandon Ramirez <brandon.ramirez@live.com>
2017-06-14 15:15:21 -0700

    List: Add keyboard navigation

--- src/List/List.js
+++ src/List/List.js
@@ -1,9 +1,12 @@
 /* @flow */
 import React from 'react'
 import Theme from 'js-theme'
+
 import { Colors, Fonts, Spacing } from '@workflo/styles'
 import Animations from '@workflo/styles/lib/Animations'
+
 import View from '../View'
+import KEYCODES from '../constants/KEYCODES'
 import ListItem from './ListItem'
 
 type SizeT = 'Small' | 'Base' | 'Large'
@@ -11,11 +14,21 @@ type SizeT = 'Small' | 'Base' | 'Large'
 type PropsT = {
   children?: React.Children,
   data: Array<any>,
+  /**
+   * Simulates keyboard navigation w/ up and down arrows. Does not actually focus
+   * the keyboard.
+   */
+  isKeyboardFocused: boolean,
   onSelect: Function,
   selectedIndex?: number,
   renderer: ReactClass<*>,
   size: SizeT,
   theme: Object,
+}
+
+type StateT = {
+  /** The index of the row in focus through keyboard navigation */
+  keyboardFocusedIndex: ?number,
 }
 
 type RendererPropsT = {
@@ -28,40 +41,121 @@ type RendererPropsT = {
 
 const defaultProps = {
   data: [],
-  renderer: ({ datum, isSelected, onClick, size, theme }: RendererPropsT) => (
-    <ListItem isSelected={isSelected} onClick={onClick} size={size} theme={theme}>
+  renderer: ({ datum, ...props }: RendererPropsT) => (
+    <ListItem {...props}>
       {datum}
     </ListItem>
   ),
   size: 'base',
 }
 
-const List = ({
-  children,
-  theme,
-  data,
-  onSelect,
-  selectedIndex,
-  renderer,
-  size,
-  ...props
-}: PropsT) => {
-  const Renderer = renderer
-  return (
-    <View {...props} {...theme.list}>
-      {data.map((datum, index) => (
-        <Renderer
-          datum={datum}
-          isSelected={index === selectedIndex}
-          key={index}
-          onClick={() => onSelect(index)}
-          size={size}
-          theme={theme}
-        />
-      ))}
-      {children}
-    </View>
-  )
+class List extends React.Component {
+  props: PropsT
+  state: StateT
+
+  static defaultProps = defaultProps
+
+  constructor(props: PropsT) {
+    const { isKeyboardFocused } = props
+    super(props)
+    this.state = {
+      keyboardFocusedIndex: isKeyboardFocused ? 0 : null,
+    }
+  }
+  componentWillReceiveProps(nextProps: PropsT) {
+    const { isKeyboardFocused } = this.props
+
+    /** Toggle keyboard navigation focus */
+    if (isKeyboardFocused !== nextProps.isKeyboardFocused) {
+      this.setState(prevState => ({
+        keyboardFocusedIndex: prevState.keyboardFocusedIndex ? 0 : null,
+      }))
+    }
+  }
+
+  componentWillMount() {
+    document.addEventListener(
+      'keydown',
+      (this.handleKeyDown: KeyboardEventListener),
+      true
+    )
+  }
+
+  componentWillUnMount() {
+    document.removeEventListener(
+      'keydown',
+      (this.handleKeyDown: KeyboardEventListener),
+      true
+    )
+  }
+
+  handleKeyDown = event => {
+    const { data } = this.props
+    if (!this.props.isKeyboardFocused) {
+      return
+    }
+    switch (event.keyCode) {
+      case KEYCODES.ArrowUp:
+        this.setState(prevState => ({
+          /** Can't keyboard navigate past the top of the list */
+          keyboardFocusedIndex: Math.max(prevState.keyboardFocusedIndex - 1, 0),
+        }))
+        break
+      case KEYCODES.ArrowDown:
+        this.setState(prevState => ({
+          /** Can't keyboard navigate past the bottom of the list */
+          keyboardFocusedIndex: Math.min(
+            prevState.keyboardFocusedIndex + 1,
+            data.length - 1
+          ),
+        }))
+        break
+      case KEYCODES.Enter:
+        this.handleSelect(this.state.keyboardFocusedIndex)
+        break
+      default:
+        return
+    }
+  }
+
+  handleSelect = index => {
+    const { onSelect } = this.props
+    if (typeof onSelect === 'function') {
+      onSelect(index)
+    }
+  }
+
+  render() {
+    const {
+      children,
+      data,
+      isKeyboardFocused, // eslint-disable-line
+      onSelect, // eslint-disable-line
+      renderer,
+      selectedIndex,
+      size,
+      theme,
+      ...props
+    } = this.props
+    const { keyboardFocusedIndex } = this.state
+    const Renderer = renderer
+    return (
+      <View {...props} {...theme.list}>
+        {data.map((datum, index) => (
+          <Renderer
+            datum={datum}
+            isKeyboardFocused={index === keyboardFocusedIndex}
+            isSelected={index === selectedIndex}
+            key={index}
+            onClick={() => this.handleSelect(index)}
+            size={size}
+            theme={theme}
+          />
+        ))}
+        {children}
+      </View>
+    )
+  }
 }
 
 const getFont = (size: SizeT) => {
@@ -94,9 +188,12 @@ const defaultTheme = ({ size }) => ({
     backgroundColor: Colors.grey300,
     padding: Spacing.tiny,
   },
+  focusedListItem: {
+    ...getFont(size),
+    backgroundColor: Colors.grey200,
+    padding: Spacing.tiny,
+  },
 })
-
-List.defaultProps = defaultProps
 
 const ThemedList = Theme('List', defaultTheme)(List)
 export default ThemedList
--- src/List/List.story.js
+++ src/List/List.story.js
@@ -4,25 +4,38 @@ import List from './List'
 import PreviewContainer from '../PreviewContainer/PreviewContainer'
 import Preview from '../Preview'
 
-storiesOf('List', module).add('Regular', () => (
-  <PreviewContainer shade="dark">
-    <Preview label="Default">
-      <List
-        onSelect={index => action('onSelect')(index)}
-        selectedIndex={2}
-        data={items}
-      />
-    </Preview>
-    <Preview label="Small">
-      <List data={items} size="Small" />
-    </Preview>
-    <Preview label="Base">
-      <List data={items} size="Base" />
-    </Preview>
-    <Preview label="Large">
-      <List data={items} size="Large" />
-    </Preview>
-  </PreviewContainer>
-))
-
 const items = ['List', 'ListItem', 'Radio', 'RadioGroup']
+
+storiesOf('List', module)
+  .add('Regular', () => (
+    <PreviewContainer shade="dark">
+      <Preview label="Default">
+        <List
+          onSelect={index => action('onSelect')(index)}
+          selectedIndex={2}
+          data={items}
+        />
+      </Preview>
+      <Preview label="Small">
+        <List data={items} size="Small" />
+      </Preview>
+      <Preview label="Base">
+        <List data={items} size="Base" />
+      </Preview>
+      <Preview label="Large">
+        <List data={items} size="Large" />
+      </Preview>
+    </PreviewContainer>
+  ))
+  .add('Keyboard Navigation', () => (
+    <PreviewContainer>
+      <Preview label="Base">
+        <List
+          data={items}
+          isKeyboardFocused
+          onSelect={action('Selected Index: ')}
+          size="Base"
+        />
+      </Preview>
+    </PreviewContainer>
+  ))
