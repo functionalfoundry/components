524a05d8803b2df17a80d72c6916d2efce695ad9
Yaniv Tal <yanivtalmusic@gmail.com>
2017-06-23 14:50:17 -0700

    Add props comments

--- src/FilledTextInput/FilledTextInput.js
+++ src/FilledTextInput/FilledTextInput.js
@@ -1,24 +1,21 @@
 import React from 'react'
-import {
-  Colors,
-  Spacing,
-} from '@workflo/styles'
+import { Colors, Spacing } from '@workflo/styles'
 import TextInput from '../TextInput'
 
 type PropsT = {
+  /* The width of the text input in pixels */
   width: number,
+  /* The value to display in the input. Must be updated on every change */
   value: string,
+  /* Called everytime the input's value is changed */
   onChange: Function,
 }
 
-const FilledTextInput = ({
-  width,
-  ...props
-}: PropsT) => (
+const FilledTextInput = ({ width, ...props }: PropsT) => (
   <TextInput
     {...props}
     disableUnderline
-    size='Base'
+    size="Base"
     theme={{
       inputContain: {
         backgroundColor: Colors.grey800,
